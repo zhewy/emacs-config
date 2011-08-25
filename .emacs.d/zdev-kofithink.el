39fa354fd956597d8ce104d20ad8bcfbaea71382
zhewy <zhewys@gmail.com>
2011-08-25 21:42:58 +1200

    python3, org file

--- .emacs.d/zdev-kofithink.el
+++ .emacs.d/zdev-kofithink.el
@@ -1,2 +1,17 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files (quote ("/media/sf_Dropbox/org/org.org"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
 (windmove-default-keybindings 'super)
 
+;;; Python Dev
+(setq python-python-command "python3")
