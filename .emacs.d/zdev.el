c1c5965e56d57c4ff71bf854a9859e75bd434eda
zhewy <zlam@dynamiccontrols.com>
2011-09-08 08:27:00 +1200

    zdev perl/python dev

--- .emacs.d/zdev.el
+++ .emacs.d/zdev.el
@@ -1,1 +1,11 @@
 (load "~/.emacs.d/dcl.el" 'noerror)
+
+;;; Python Dev
+(setq python-python-command "python3")
+(setq python-mode-hook (function cscope:hook))
+
+;;; Perl Dev
+;;; cperl-mode is preferred to perl-mode                 
+(defalias 'perl-mode 'cperl-mode)
+(setq cperl-mode-hook (function cscope:hook))
+(require 'perldoc)
