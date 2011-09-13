3d2f8d2edb221cb2377f39439e3d287f632557ee
zhewy <zhewy@zdev-unishark.au.ivc>
2011-09-14 09:03:13 +1200

    unidrive dev computer default org file

--- .emacs.d/zdev-unishark.el
+++ .emacs.d/zdev-unishark.el
@@ -1,1 +1,3 @@
+(setq org-agenda-files (quote ("~/NETWORK/hdrive/org/unidrive.org")))
+
 (load "~/.emacs.d/dcl.el" 'noerror)
--- .emacs.d/zdev.el
+++ .emacs.d/zdev.el
@@ -6,7 +6,6 @@
  '(default ((t (:inherit nil :stipple nil :background "white" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 102 :width normal :foundry "unknown" :family "DejaVu Sans Mono")))))
 
 (setq org-agenda-files (quote ("~/NETWORK/hdrive/org/dc.org" "~/NETWORK/hdrive/org/unidrive.org")))
-(setq tool-bar-mode nil)
 
 ;;; Python Dev
 (setq python-python-command "python3")
