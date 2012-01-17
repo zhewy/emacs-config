fccbd52b7996eaa7979b45fa9611eefe5e8c2041
zhewy <zhewys@gmail.com>
2012-01-17 19:48:46 +1300

    win xp specific file

---
+++ .emacs.d/winxp.el
@@ -0,0 +1,8 @@
+;; setting the PC keyboard's various keys to
+;; Super or Hyper, for emacs running on Windows.
+(setq w32-pass-lwindow-to-system nil 
+      w32-pass-rwindow-to-system nil 
+      w32-pass-apps-to-system nil 
+      w32-lwindow-modifier 'super ; Left Windows key 
+      w32-rwindow-modifier 'super ; Right Windows key 
+      w32-apps-modifier 'hyper) ; Menu key
