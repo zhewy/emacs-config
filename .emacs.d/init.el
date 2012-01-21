14445a974ae93cc5a35d62c7e42036bbb398fd56
zhewy <zhewys@gmail.com>
2012-01-22 06:57:07 +1300

    allow generic org mode agenda files

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -6,6 +6,19 @@
 
 (defvar *emacs-load-start* (current-time))
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files "~/Dropbox/org/agenda-files"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
 ;; setup variables
 (setq dotfiles-dir (file-name-directory
 		    (or (buffer-file-name) load-file-name)))
--- .emacs.d/kofithink.el
+++ .emacs.d/kofithink.el
@@ -1,16 +1,3 @@
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(org-agenda-files (quote ("~/Dropbox/dcl/personal.org" "~/Dropbox/org/org.org"))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
 (load "~/.emacs.d/win-general.el")
 
 (windmove-default-keybindings 'control)
