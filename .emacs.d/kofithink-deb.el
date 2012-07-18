c4c9d399c08f5d4051ea797685c96fbc1ddf8563
zhewy <zhewy@kofithink-deb.kofihouse>
2012-07-18 23:23:19 +1200

    yasnippet

--- .emacs.d/kofithink-deb.el
+++ .emacs.d/kofithink-deb.el
@@ -24,3 +24,8 @@
 
 (add-to-list 'load-path "~/.emacs.d/")
 (require 'taskjuggler-mode)
+
+(add-to-list 'load-path "~/.emacs.d/plugins/yasnippet-0.6.1c")
+(require 'yasnippet)
+(yas/initialize)
+(yas/load-directory "~/.emacs.d/plugins/yasnippet-0.6.1c/snippets")
